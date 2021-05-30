c0b4f15fd06c7c570e0c095ca4dc580644163329
MichalKonvic <51423281+MichalKonvic@users.noreply.github.com>
2021-05-30 21:46:37 +0200

    Css compiler

--- Nitrack.js
+++ Nitrack.js
@@ -2,27 +2,7 @@
     Created by MichalKonvic
     Nitrack is the next ui library
 */
-class view{
-    #HTML = ``;
-    #CSS = {};
-    js = ``;
-    /**
-     * 
-     * @param {String} htmlContent 
-     * @return {Boolean} true/false if html check passed
-     */
-    html(htmlContent){
-        //html check
-    }
-    /**
-     * 
-     * @param {String} cssContent 
-     * @return {Boolean} true/false if css check passed
-     */
-    css(cssContent){
-        //Css check
-    }
-}
+
 class nitrack{
     /**
      * 
@@ -32,6 +12,7 @@ class nitrack{
     render(renderContent,renderElementID){
         try {
             document.getElementById(renderElementID).innerHTML = renderContent;
+
         } catch (error) {
             if (error == "TypeError: document.getElementById(...) is null") {
                 console.log(`The %c${renderElementID}%c is not valid id!\n Error moved to error panel!`,"color:red;","color:white;");
@@ -41,15 +22,32 @@ class nitrack{
             }
         }
     }
+    /**
+     * 
+     * @param {Object}  NitrackViewObject
+     * @param {String} renderElementID 
+     */
     renderView(viewObject,renderElementID){
         try {
-                    // Render HTML
-
-                    // Render css
-
-                    // Render JS
+                // Render HTML
+                document.getElementById(renderElementID).innerHTML = viewObject.HTML;
         } catch (error) {
-            
+            console.error("Error occured while rendering html!");
+        }
+        try {
+            // Render CSS
+            let stylesObject = viewObject.getStyles();
+            // Loops through elements
+            Object.keys(styles).forEach(elementId =>{
+                const styles = stylesObject[elementId];
+                //  Loops through element styles
+                Object.keys(styles).forEach(style => {
+                    // Renders style
+                    document.getElementById(elementId)[style] = styles[style];
+                });
+            });
+        } catch (error) {
+            console.error("Error occured while rendering css!");
         }
     }
 }
