75cfc37543f192d77f0c1707137ae9195efa75c4
MichalKonvic <51423281+MichalKonvic@users.noreply.github.com>
2021-06-11 23:00:36 +0200

    Working css,html reder

--- Nitrack.js
+++ Nitrack.js
@@ -2,8 +2,28 @@
     Created by MichalKonvic
     Nitrack is the next ui library
 */
+/**
+ * Converts css style to js style name
+ * @param {string} styleName in css
+ * @returns return css style converted to js style, undefined when css style is invalid
+ */
+function cssToJs(styleName=""){
+    if(styleName.length > 1){
+        try {
+            styleName = styleName.split("-");
+            for (let letter = 0; letter < styleName.length; letter++) {
+                if(letter == 0) continue;
+                styleName[letter] = styleName[letter][0].toUpperCase() + styleName[letter].slice(1);
+            }
+            styleName = styleName.join("");
+        } catch (error) {
+            return undefined;
+        }
+    }
+    return styleName
+}
 
-class nitrack{
+export class nitrack{
     /**
      * 
      * @param {string} renderContent 
@@ -12,7 +32,6 @@ class nitrack{
     render(renderContent,renderElementID){
         try {
             document.getElementById(renderElementID).innerHTML = renderContent;
-
         } catch (error) {
             if (error == "TypeError: document.getElementById(...) is null") {
                 console.log(`The %c${renderElementID}%c is not valid id!\n Error moved to error panel!`,"color:red;","color:white;");
@@ -29,25 +48,83 @@ class nitrack{
      */
     renderView(viewObject,renderElementID){
         try {
-                // Render HTML
-                document.getElementById(renderElementID).innerHTML = viewObject.HTML;
+            document.getElementById(renderElementID).innerHTML = viewObject.HTML; // Render HTML
         } catch (error) {
             console.error("Error occured while rendering html!");
         }
         try {
             // Render CSS
-            let stylesObject = viewObject.getStyles();
-            // Loops through elements
-            Object.keys(styles).forEach(elementId =>{
-                const styles = stylesObject[elementId];
-                //  Loops through element styles
-                Object.keys(styles).forEach(style => {
-                    // Renders style
-                    document.getElementById(elementId)[style] = styles[style];
-                });
-            });
+            Object.entries(viewObject.CSS).forEach(([elementId,elementStyles]) => {
+                Object.entries(elementStyles).forEach(([cssStyle,cssStyleValue]) => {
+                    document.getElementById(elementId).style[cssToJs(cssStyle)] = cssStyleValue;
+                })
+            })
         } catch (error) {
             console.error("Error occured while rendering css!");
+            console.log(error)
         }
+        try {
+            document.getElementById(renderElementID).innerHTML += viewObject.jsCompile();
+        } catch (error) {
+            console.error("Error occured while rendering javascript!");
+        }
+    }
+}
+export class nitrackView{
+    HTML = ``;
+    CSS = {};
+    jsCompileProperties = {
+        "async": "__!Include",
+        "crossorigin=": "__!Include",
+        "integrity=": "__!Include",
+        "nomodule": "__!Include",
+        "referrerpolicy=": "__!Include",
+        "src=": "__!Include",
+        "type=": "__!Include"
+    };
+    js = () => {
+
+    };
+    /**
+     * Adds or changes element style.
+     * @param {String} elementId 
+     * @param {Object} styles !!!PUT EVERYTHING INSIDE OBJECT TO STRING!!!
+     * @returns Updated css Object
+     */
+    newStyle(elementId,styles){
+        this.CSS[elementId] = styles;
+        return this.CSS;
+    }
+    /**
+     * Removes element style
+     * @param {String} elementId 
+     * @returns Updated css Object or if element doesnt exist undefined
+     */
+    removeElementStyle(elementId){
+        if (!this.CSS[elementId] || elementId === undefined) {
+            return undefined;
+        }
+        delete this.CSS[elementId];
+        return this.CSS;
+    }
+    /**
+     * JavaScript code that will be rendered with view
+     * @param {Function} jsCode 
+     */
+    setJS(jsCode){
+        this.js = jsCode();
+    }
+    jsCompile(){
+        let properties = [];
+        Object.entries(this.jsCompileProperties).forEach(([key, value]) => {
+            if(value === "__!Include"){
+            }else if(key.charAt(key.length - 1) !== "="){
+                properties.push(key);
+            }else{
+                properties.push(`${key}"${value}"`);
+            }
+        });
+        properties = properties.join(" ");
+        return `<script ${properties}>${this.js}</script>`;
     }
 }
--- README.md
+++ README.md
@@ -1,9 +1,64 @@
-# Nitrack
+# Nitrack #
+
 Another ui library.
 Created by absolute beginner ([me🖐](https://github.com/MichalKonvic)).
 
-# Installation
- * Copy `Nitrack.js` into your project folder
- * Add `import 'Nitrack.js'` line
- * Add `let Nitrack = new NITRACK();` line
- * Read docs: (Docs are not created yet.).
+## Installation ##
+
+---
+
+* Copy `Nitrack.js` into your project folder
+* Add `import 'Nitrack.js'` line
+* Add `let Nitrack = new nitrack();` line
+* Read [docs](##Docs).
+
+## Docs ##
+
+---
+
+### Nitrack-View ###
+
+Example of Nitrack-View:
+```javascript
+import nitrackView from "./Nitrack-View";
+
+let demoView = new nitrackView();
+demoView.HTML = `
+    <h1 id="Title">Hello world</h1>
+    <p id="Description">This text is rendered using NitrackView </p>
+`;
+
+/* Styles for Title */
+demoView.newStyle("Title",{
+    "font-size":"100px",
+    "color":"red"
+});
+
+/* Styles for Description */
+demoView.newStyle("Description",{
+    "font-size":"60px",
+    "color":"green"
+});
+
+/* Scripts are rendered as <script> tag inside browser */
+demoView.setJS(()=>{
+    console.log("Script loaded");
+})
+
+/* This properties are optional */
+demoView.jsCompileProperties = {
+    "async": true,
+    "type=": "application/javascript"
+};
+/* Default jsCompileProperties:
+    "async": "__!Include",
+    "crossorigin=": "__!Include",
+    "integrity=": "__!Include",
+    "nomodule": "__!Include",
+    "referrerpolicy=": "__!Include",
+    "src=": "__!Include",
+    "type=": "__!Include"
+*/
+
+export default demoView;
+```
